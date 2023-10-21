d744512e12dfbc27b91058b80bf3e57a00551c5c
Marharyta Parkalava <margobraun777@gmail.com>
2023-10-21 04:43:41 +0300

    fix: welldone brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const open = bracketsConfig.map(pair => pair[0]);
+  const close = bracketsConfig.map(pair => pair[1]);
+  const matchingPairs = {};
+
+  for (const pair of bracketsConfig) {
+    const [openingBracket, closingBracket] = pair;
+    matchingPairs[closingBracket] = openingBracket;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i];
+
+    if (open.includes(currentChar)) {
+      if (currentChar === matchingPairs[currentChar] && stack[stack.length - 1] === currentChar) {
+        stack.pop();
+      } else {
+        stack.push(currentChar);
+      }
+    } else if (close.includes(currentChar)) {
+      const lastItem = stack.pop();
+      if (!lastItem || lastItem !== matchingPairs[currentChar]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
